2da4cae260db5099e2ae22be5040ecbbcb55ad31
abhi-s7 <abhishek.dev1697@gmail.com>
2026-01-18 12:09:07 -0800

    Removing unused imports

--- src/main/java/com/abhi/aws/ec2/controller/WelcomeController.java
+++ src/main/java/com/abhi/aws/ec2/controller/WelcomeController.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 @Controller
 public class WelcomeController {
 
